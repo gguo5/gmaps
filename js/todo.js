cd97b607e15ed0d8d4d69e3bc33c898eecd373e7
gguo <gguo@KRIS-PC.dataagility.com>
2013-06-18 10:30:55 +1000

    add todo.js

---
+++ js/todo.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function TodoCtrl($scope) {
+  $scope.todos = [
+    {text:'learn angular', done:false},
+    {text:'build an angular app', done:false},
+    {text:'skydiving', done:true},
+    {text:'scuba diving', done:true}];
+
+  $scope.addTodo = function() {
+    $scope.todos.push({text:$scope.todoText, done:false});
+    $scope.todoText = '';
+  };
+
+  $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+
+  $scope.archive = function() {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    });
+  };
+}
+
